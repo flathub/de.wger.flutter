21ea1f0227c4b3e10d464113026198dc4149a6e2
Roland Geider <roland@geider.net>
2025-06-17 22:34:05 +0200

    Some renaming, formatting and cleanup

--- bump-wger-version.py
+++ bump-wger-version.py
@@ -1,7 +1,21 @@
+# This file is part of wger Workout Manager.
+#
+# wger Workout Manager is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# wger Workout Manager is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+
 import json
 import sys
 
-def update_flutter_version(filename: str, new_version: str):
+def update_wger_version(filename: str, new_version: str):
     """Updates the version of the wger flutter source in the flatpak-flutter JSON file."""
 
     with open(filename, 'r', encoding="utf-8") as f:
@@ -31,5 +45,5 @@ if __name__ == "__main__":
         sys.exit(1)
 
     version = sys.argv[1]
-    update_flutter_version("flatpak-flutter.json", version)
+    update_wger_version("flatpak-flutter.json", version)
     print(f"Version updated to {version}.")
